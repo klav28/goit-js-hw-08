76734d4f77ee0e231186c950e9ffa913898f1ac2
klav28 <klav28@gmail.com>
2023-03-12 22:22:11 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const formData = document.querySelector(".feedback-form");
 
+import throttle from "lodash.throttle";
+
+const feedbackFormValues = localStorage.getItem("feedback-form-state");
+
+if (feedbackFormValues) {
+    try {
+        const storedValues = JSON.parse(feedbackFormValues);
+        formData.elements.email.value = storedValues.email;
+        formData.elements.message.value = storedValues.message;
+    } catch (error) {
+        console.error("Get state error: ", error.message);
+    }
+}
+
+function handleInput(event) {
+    const { email, message } = event.currentTarget.elements;
+    localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }));
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const { email, message } = event.currentTarget.elements;
+    console.log({ email: email.value, message: message.value });
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+formData.addEventListener("input", throttle(handleInput, 1500));
+
+formData.addEventListener("submit", handleSubmit);
